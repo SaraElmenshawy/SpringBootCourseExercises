44d98bdf668dba9d7c3581b61f871104c5cb6864
Sara Elmenshawy <saraelmenshawy273@gmail.com>
2017-06-26 01:36:35 +0200

    Implementing Put and Delete Requests.

--- course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -25,9 +25,30 @@ public class TopicService
 		return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
 	}
 
-	public void addTopic(Topic topic) {
+	public void addTopic(Topic topic)
+	{
 		topics.add(topic);
 		
 	}
+
+	public void updateTopic(String id, Topic topic) 
+	{
+		for(int i=0;i<topics.size();i++)
+		{
+			Topic t=topics.get(i);
+			if(t.getId().equals(id))
+			{
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+
+	public void deleteTopic(String id) 
+	{
+		topics.removeIf(t->t.getId().equals(id));
+		
+	}
+
 	
 }
